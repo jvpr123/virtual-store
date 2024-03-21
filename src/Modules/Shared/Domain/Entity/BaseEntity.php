366c37b426954b0f18fd5eb511b0d73212393f94
João Victor Pereira Rodrigues <jvpr123@outlook.com>
2024-03-21 13:15:33 -0300

    feat: create shared kernel

---
+++ src/Modules/Shared/Domain/Entity/BaseEntity.php
@@ -0,0 +1,31 @@
+<?php
+
+abstract class BaseEntity
+{
+    public function __construct(
+        private int $_id,
+        private DateTime $_createdAt,
+        private DateTime $_updatedAt,
+    ) {
+    }
+
+    public function getID(): int
+    {
+        return $this->_id;
+    }
+
+    public function getCreatedAt(): DateTime
+    {
+        return $this->_createdAt;
+    }
+
+    public function getUpdatedAt(): DateTime
+    {
+        return $this->_updatedAt;
+    }
+
+    public function setUpdatedAt(DateTime $updatedAt): void
+    {
+        $this->_updatedAt = $updatedAt;
+    }
+}
